2dd70f50d1a648b4b8192475462101b867a78ea7
Elias Tamer <eliastamer3@gmail.com>
2024-05-24 16:13:12 +0300

    finalized the script

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import requests
 from send_email import send_email
 
-api_key="YOUR_API_KEY_HERE"
-url = f"https://newsapi.org/v2/everything?q=tesla&from=2024-04-23&" \
-"sortBy=publishedAt&apiKey=${api_key}"
+
+topic = input("Select a topic that you would like to receive daily news for:")
+
+api_key="KEY HERE"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2024-04-24&" \
+    f"sortBy=publishedAt&apiKey={api_key}&language=en"
 
 request = requests.get(url)
 content = request.json()
 
 body = ""
 
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+        body = body + article["title"] + "\n" \
+        + article["description"]  + "\n" \
+        + article["url"] + 2 * "\n"
     
     
-send_email(subject="News Mailer" , body=body)
+send_email(subject="Today's News" , body=body)
